2771b3a83d39554eed49d449a624d7c0a36f5486
PedroHGMelim <phgmelim@gmail.com>
2026-05-12 15:17:44 -0300

    Tela de Login

---
+++ src/components/LoginScreen.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/LoginScreen.css';
+import logo from '../assets/logo_toque-final-104h.png';
+
+function Login() {
+    const [email, setEmail] = useState(() => {
+        const emailSalvo = localStorage.getItem('emailSalvo');
+        return emailSalvo ? JSON.parse(emailSalvo) : '';
+    });
+
+    const [password, setPassword] = useState('');
+
+    useEffect(() => {
+        localStorage.setItem('emailSalvo', JSON.stringify(email));
+    }, [email]);
+
+    return (
+        <div className="login-page">
+
+            <header className="login-header">
+                <img
+                    src={logo}
+                    alt="logo toque final gesso"
+                    className="company-logo"
+                />
+            </header>
+
+            <main className="login-container">
+
+                <form className="login-form">
+
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder="Digite seu email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="password">Senha</label>
+
+                        <input
+                            id="password"
+                            type="password"
+                            placeholder="Digite sua senha"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="login-button"
+                    >
+                        Entrar
+                    </button>
+
+                </form>
+
+            </main>
+
+            <footer className="login-footer">
+                <a href="./RegisterScreen.jsx">
+                    Não tem uma conta? Registre-se aqui
+                </a>
+            </footer>
+
+        </div>
+    );
+}
+
+export default Login;
